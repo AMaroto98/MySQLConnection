da63fd380066b49e7a3aa1f2a6e82d42350fbb94
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-04-19 12:17:21 +0200

    Código refactorizado

--- JDBCPool/MySQLConnection/src/main/java/MySQLConnection/DataBase.java
+++ JDBCPool/MySQLConnection/src/main/java/MySQLConnection/DataBase.java
@@ -7,196 +7,115 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class DataBase {
 
-    public static void insertGame(Game game) {
-
-        String url = "jdbc:mysql://localhost/Game";
-        String user = "root";
-        String password = "1234";
-    
-        HikariDataSource dataSource = null;
-        Connection connection = null;
-    
-        try {
-
-            dataSource = new HikariDataSource();
-            dataSource.setJdbcUrl(url);
-            dataSource.setUsername(user);
-            dataSource.setPassword(password);
-    
-            connection = dataSource.getConnection();
-
-        } catch (SQLException e) {
-
-            System.err.println("Connection error: " + e.getMessage());
-        }
-    
-        Statement st = null;
+    public static void closeConnection(HikariDataSource dataSource, Connection connection) {
 
         try {
 
-            st = connection.createStatement();
+            if (dataSource != null) {
+                dataSource.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
 
         } catch (SQLException e) {
 
-            System.out.println("Statement error: " + e.getMessage());
-
-        }
-    
-        String query = "INSERT INTO Game VALUES ('" + game.getname() + "'," + game.getyear() + ");";
-    
-        try {
-
-            st.executeUpdate(query);
-
-        } catch (SQLException e) {
-
-            System.out.println("ExecuteUpdate error: " + e.getMessage());
-
-        } 
-    
-        try {
-
-            dataSource.close();
-            connection.close(); // Cerrar conexión
-
-        } catch (SQLException e) {
-
-            System.err.println("Close error: " + e.getMessage());
+            System.err.println("Error closing connection: " + e.getMessage());
         }
     }
     
+    public static void insertGame(Game game) {
 
-    public static void deleteGame(String gameName) {
-
-        String url = "jdbc:mysql://localhost/Game";
-        String user = "root";
-        String password = "1234";
-    
         HikariDataSource dataSource = null;
         Connection connection = null;
+        Statement statement = null;
     
         try {
 
             dataSource = new HikariDataSource();
-            dataSource.setJdbcUrl(url);
-            dataSource.setUsername(user);
-            dataSource.setPassword(password);
+            dataSource.setJdbcUrl("jdbc:mysql://localhost/Game");
+            dataSource.setUsername("root");
+            dataSource.setPassword("1234");
     
             connection = dataSource.getConnection();
-
-        } catch (SQLException e) {
-
-            System.err.println("Connection error: " + e.getMessage());
-        }
-
-        Statement st = null;
-
-        try {
-
-            st = connection.createStatement();
-
-        } catch (SQLException e) {
-
-            System.out.println("Statement error: " + e.getMessage());
-
-        }
-
-
-        String query = "DELETE FROM Game WHERE Name = " + "'" +  gameName + "'";
-
-        try {
+            statement = connection.createStatement();
+        
+            String query = "INSERT INTO Game VALUES ('" + game.getname() + "'," + game.getyear() + ");";
+            statement.executeUpdate(query);
             
-            st.executeUpdate(query);
-            System.out.println(gameName + " successfully eliminated");
-
         } catch (SQLException e) {
 
-            System.out.println("ExecuteQuery error: " + e.getMessage());
-        }
+            System.out.println("ExecuteUpdate error: " + e.getMessage());
+            
+        } finally {
 
+            closeConnection(dataSource, connection);
+        }
+    }
+
+    public static void deleteGame(String gameName) {
+
+        HikariDataSource dataSource = null;
+        Connection connection = null;
+        Statement statement = null;
+    
         try {
 
-            dataSource.close();
-            connection.close(); // Cerrar conexión
-
+            dataSource = new HikariDataSource();
+            dataSource.setJdbcUrl("jdbc:mysql://localhost/Game");
+            dataSource.setUsername("root");
+            dataSource.setPassword("1234");
+    
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+        
+            String query = "DELETE FROM Game WHERE Name = " + "'" +  gameName + "'";
+            statement.executeUpdate(query);
+            
         } catch (SQLException e) {
 
-            System.err.println("Close error: " + e.getMessage());
-        }
+            System.out.println("ExecuteUpdate error: " + e.getMessage());
+            
+        } finally {
 
+            closeConnection(dataSource, connection);
+        }
     }
 
     public static void showTable() {
 
-        String url = "jdbc:mysql://localhost/Game";
-        String user = "root";
-        String password = "1234";
-    
         HikariDataSource dataSource = null;
         Connection connection = null;
+        Statement statement = null;
     
         try {
 
             dataSource = new HikariDataSource();
-            dataSource.setJdbcUrl(url);
-            dataSource.setUsername(user);
-            dataSource.setPassword(password);
+            dataSource.setJdbcUrl("jdbc:mysql://localhost/Game");
+            dataSource.setUsername("root");
+            dataSource.setPassword("1234");
     
             connection = dataSource.getConnection();
+            statement = connection.createStatement();
+        
+            String query = "SELECT * FROM Game;";
+            ResultSet rs = statement.executeQuery(query);
 
-        } catch (SQLException e) {
-
-            System.err.println("Connection error: " + e.getMessage());
-        }
-
-        // Creación del Statement
-        Statement st = null;
-
-        try {
-            
-            st = connection.createStatement();
-
-        } catch (SQLException e) {
-
-            System.err.println("Statement error: " + e.getMessage());
-        }
-
-        String query = "SELECT * FROM Game;";
-
-        try {
-
-            // ResultSet nos permite almacenar cualquier cosa que nos devuelve la BBDD.
-            // ResultSet es un tipo de array pero no podemos acceder por el índice
-            // Por ellos usamos next();
-            ResultSet rs = st.executeQuery(query);
-
-            // next() devuelve true si hay algun elemento en la posición que esta mirando
-            // Con el next recorre la array entera
             while (rs.next()) {
 
-                // Cuando entra de un campo de la array se "centra" en el y podemos acceder a cada campo del registro
-                // Esto devuelve el nombre de la primera posición, se pone getString porque en la BBDD el campo nom es un String.
                 System.out.println("Game name: " + rs.getString("Name"));
-
-                // Los mismo con el getInt.
                 System.out.println("Game year: " + rs.getInt("Year"));
                 
             }
-
-
+            
         } catch (SQLException e) {
 
-            System.err.println("ExecuteQuery error: " + e.getMessage());
+            System.out.println("ExecuteUpdate error: " + e.getMessage());
+            
+        } finally {
+
+            closeConnection(dataSource, connection);
         }
-
-        try {
-
-            dataSource.close();
-            connection.close(); // Cerrar conexión
-
-        } catch (SQLException e) {
-
-            System.err.println("Close error: " + e.getMessage());
-        }
-    } 
+    }      
 }
